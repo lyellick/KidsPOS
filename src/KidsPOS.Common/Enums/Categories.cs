58ebaa56dfe33086207c4a6045f3c6826853fe99
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-06-18 20:47:58 -0500

    Create Categories.cs

---
+++ src/KidsPOS.Common/Enums/Categories.cs
@@ -0,0 +1,17 @@
+﻿namespace KidsPOS.Common.Enums
+{
+    public enum Categories
+    {
+        Beverages,
+        Bread,
+        Canned,
+        Dairy,
+        Dry,
+        Frozen,
+        Meat,
+        Produce,
+        Cleaners,
+        Toiletries,
+        Other
+    }
+}
