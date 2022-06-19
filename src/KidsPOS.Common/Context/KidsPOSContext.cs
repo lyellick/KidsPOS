cc7d02bc35c12f820bf2357547f222be97ae0d68
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-06-18 20:33:42 -0500

    Create KidsPOSContext.cs

---
+++ src/KidsPOS.Common/Context/KidsPOSContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace KidsPOS.Common.Context
+{
+    public class KidsPOSContext : DbContext
+    {
+        protected KidsPOSContext()
+        {
+        }
+
+        public KidsPOSContext(DbContextOptions<KidsPOSContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (IMutableEntityType entityType in builder.Model.GetEntityTypes())
+                builder.Entity(entityType.ClrType).ToTable(entityType.ClrType.Name);
+        }
+    }
+}
