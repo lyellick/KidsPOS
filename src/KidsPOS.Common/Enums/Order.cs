abf43fc8e8e6ed1f51832ae9425fe4fb1a01ff8d
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-06-18 21:01:13 -0500

    Create Order.cs

---
+++ src/KidsPOS.Common/Enums/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace KidsPOS.Common.Enums
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public DateTimeOffset Created { get; set; }
+
+        public Guid OrderGuid { get; set; }
+    }
+}
